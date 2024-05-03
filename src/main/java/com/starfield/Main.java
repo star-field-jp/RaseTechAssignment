c110cdf8caf776e2e538e3c645b2dea300dbb7d4
star-field-jp <hoshigari728@gmail.com>
2024-05-04 03:41:27 +0900

    feat: #4 Product.java and modify Main.java to use STREAM grouping code.

--- src/main/java/com/starfield/Main.java
+++ src/main/java/com/starfield/Main.java
@@ -1,34 +1,43 @@
 package com.starfield;
 
+
+import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
-public class Main {
-    public static void main(String[] args) {
 
-        List<Integer> list = new ArrayList<>();
+class Main {
+public static void main(String[] args) {
+    List<Product> prdList = new ArrayList<>();
+    prdList.add(new Product("1", "iPhone X","iPhone",BigDecimal.valueOf(120000),2));
+    prdList.add(new Product("2", "iPhone 8 Plus","iPhone", BigDecimal.valueOf(110000),3));
+    prdList.add(new Product("3", "iPhone 8","iPhone",BigDecimal.valueOf(100000),1));
+    prdList.add(new Product("4", "Galaxy S9","Android",BigDecimal.valueOf(100000),4));
+    prdList.add(new Product("5", "Galaxy S9 plus","Android",BigDecimal.valueOf(110000),3));
+    prdList.add(new Product("6", "Windows phone","Others",BigDecimal.valueOf(80000),1));
+    prdList.add(new Product("7", "Windows phone","Others",BigDecimal.valueOf(85000),2));
 
-        list.add(1);
-        list.add(2);
-        list.add(3);
+    // 複合キーを作成
+    Function<Product, String> compositeKey = prd -> {
+        StringBuffer sb = new StringBuffer();
+        sb.append(prd.getProductType()).append("-").append(prd.getProductName());
+        return sb.toString();
+    };
 
-        //例外(IndexOutOfBoundsException)を追加
-        list.set(3, 4);
+    // 複合キーでグルーピング
+    Map<String, List<Product>> grpByComplexKeys = prdList.stream().collect(
+            Collectors.groupingBy(compositeKey));
+    System.out.println(grpByComplexKeys);
 
-        System.out.println(list);
+    // BigDeciamlの合計
+    Map<Object, BigDecimal> grpByTypeSum = prdList.stream().collect(
+            Collectors.groupingBy(Product::getProductType,
+                    Collectors.reducing(BigDecimal.ZERO, Product::getPrice, BigDecimal::add)));
+    System.out.println(grpByTypeSum);
 
-        Map<String, Integer> map = new HashMap<>();
-        map.put("apple", 1);
-        map.put("orange", 2);
-        map.put("melon", 3);
+}
 
-        System.out.println(map);
-
-        List<String> list2 = new ArrayList<>(map.keySet());
-
-        System.out.println(list2);
-
-    }
 }
